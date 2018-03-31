0236b93ce4c704aa1ad874075d5fdc3fded3a60f
Andrew Rozhenko <andrew.r224@gmail.com>
2018-03-31 13:26:45 +0300

    Wire up application :chocolate_bar:

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const gameBoard = (() => {
-  const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  let xBoxes  = [];
-  let oBoxes  = [];
+  let board  = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  let xBoxes = [];
+  let oBoxes = [];
 
   let winningCombos = [
     [0, 1, 2],
@@ -14,60 +14,95 @@ const gameBoard = (() => {
     [2, 4, 6]
   ];
 
-  let whichPlayer = (sign) => {
-    return sign === 'X' ? xBoxes : oBoxes;
-  };
+  let whichPlayer = (sign) => { return sign === 'X' ? xBoxes : oBoxes; };
 
-  let valid = (box) => {
-    return board.includes(box) && !xBoxes.includes(box) && !oBoxes.includes(box);
-  };
+  let valid = (box) => { return board.includes(box); };
 
   let reset = () => {
     xBoxes.length = 0;
     oBoxes.length = 0;
+    board.length  = 0;
+
+    for (let i = 0; i < 9; i++) { board.push(i); }
+
+    return true;
   };
 
   let claim = (sign, box) => {
     if (valid(box)) {
-      let playerBoxes = whichPlayer(sign);
+      whichPlayer(sign).push(box);
 
-      playerBoxes.push(box);
+      board.splice(board.indexOf(box), 1);
 
       return true;
     }
+
     return false;
   };
 
-  let checkVictory = (sign) => {
-    let victory = false;
+  let checkIfOver = (players) => {
+    return won(players[1].sign) || tie(players[0].sign);
+  };
+
+  let won = (sign) => {
     let playerBoxes = whichPlayer(sign);
 
-    if (playerBoxes .length < 3) { return victory; }
+    if (playerBoxes .length < 3) { return false; }
 
     for (let i = 0; i < winningCombos.length; i++) {
       let combo = winningCombos[i];
 
       combo = combo.filter(b => !playerBoxes.includes(b));
 
-      if (combo.length === 0) { victory = true; }
+      if (combo.length === 0) {
+        board = [];
+
+        interactor.notify(`Player ${sign} has won!`);
+
+        return true;
+      }
     }
 
-    return victory;
+    return false;
   };
 
-  return { board, xBoxes, oBoxes, reset, claim, checkVictory };
+  let tie = (sign) => {
+    let tie = true;
+
+    if (board.length > 1) { return false; }
+    if (board.length == 0) { return true; }
+
+    let playerBoxes = whichPlayer(sign);
+    let otherBoxes  = playerBoxes.slice(0);
+
+    otherBoxes.push(board[0]);
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      let combo = winningCombos[i];
+
+      combo = combo.filter(b => !otherBoxes.includes(b));
+
+      if (combo.length === 0) { return false; }
+    }
+
+    board = [];
+
+    interactor.notify("It's a tie");
+
+    return true;
+  };
+
+  let render = () => {
+    interactor.renderBoard(board);
+  };
+
+  return { valid, reset, render, claim, checkIfOver };
 })();
 
 class Player {
-  constructor(sign) {
+  constructor(sign, name) {
     this.sign = sign;
-  }
-
-  get sign() { return this._sign; };
-  set sign(newSign) { this._sign = newSign; };
-
-  getInput() {
-    return parseInt(prompt(`Player ${this.sign}, please choose a box (0-8)`));
+    this.name = name;
   }
 
   static new(params) {
@@ -77,32 +112,110 @@ class Player {
 
 const controller = (() => {
   let gameOver = false;
+  let players;
+  let board;
 
-  let play = (players) => {
-    while (!gameOver) {
-      let player = players.shift();
-      let input;
-
-      while (!gameBoard.claim(player.sign, input)) {
-        input = player.getInput();
-      }
-
-      gameOver = gameBoard.checkVictory(player.sign);
-
-      players.push(player);
-    }
-
-    console.log(`Player ${players[1].sign} has won!`);
+  let setup = (params) => {
+    players = params['players'];
+    board   = params['board'];
   };
 
-  return { play };
+  let play = (square) => {
+    let player = players.shift();
+    let sign   = player.sign;
+
+    if (!gameBoard.claim(sign, square)) {
+      players.unshift(player);
+
+      return false;
+    }
+
+    interactor.notify(`Player ${players[0].sign}'s turn`);
+
+    players.push(player);
+
+    gameOver = gameBoard.checkIfOver(players);
+
+    return sign;
+  };
+
+  return { setup, play, players };
 })();
 
+const interactor = (() => {
+  let board       = document.querySelector('#board');
+  let noticeArea  = document.querySelector('#notice');
+  let buttonGroup = document.querySelector('.button-group');
+
+  let renderBoard = (boxes) => {
+    clearBoard();
+
+    for (let i = 0; i < boxes.length; i++) { board.appendChild(buildBox(boxes[i])); }
+
+    let resetButton = document.createElement('button');
+    resetButton.classList.add('reset');
+    resetButton.textContent = 'Reset';
+    resetButton.addEventListener('click', reset);
+
+    buttonGroup.appendChild(resetButton);
+    notify(`Player ${players[0].sign}'s turn`);
+  };
+
+  let notify = (msg) => { noticeArea.textContent = msg; };
+
+  let clearBoard = () => {
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
+    }
+
+    let resetButton = document.querySelector('.reset');
+
+    if (resetButton) { buttonGroup.removeChild(resetButton); }
+  };
+
+  let buildBox = (val) => {
+    let box = document.createElement('div');
+
+    box.addEventListener('click', play);
+    box.addEventListener('mouseover', checkBox);
+    box.addEventListener('mouseout', restoreBox);
+
+    box.dataset.value = val;
+
+    return box;
+  };
+
+  function checkBox() {
+    let box = parseInt(this.getAttribute('data-value'));
+
+    gameBoard.valid(box) ? this.classList.add('free') : this.classList.add('occupied');
+  }
+
+  function play() {
+    let box = parseInt(this.getAttribute('data-value'));
+
+    let sign = controller.play(box);
+
+    if (sign) {
+      this.textContent = sign;
+      this.dataset.value = sign;
+    }
+  };
+
+  function reset() { gameBoard.reset() && gameBoard.render(); };
+
+  function set() { buttonGroup.removeChild(newButton) && gameBoard.render(); }
+
+  function restoreBox() { this.className = ''; };
+
+  return { set, reset, renderBoard, notify };
+})();
+
+let newButton = document.querySelector('.button-group .new');
 let x = Player.new('X');
 let o = Player.new('O');
+let players = [x, o];
 
-// Play:
-// controller.play([x, o]);
+controller.setup({ 'players': players, 'board': gameBoard });
 
-// Reset board:
-// gameBoard.reset();
+newButton.addEventListener('click', interactor.set);
